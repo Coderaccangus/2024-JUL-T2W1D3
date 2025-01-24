6e6eb981b5845013756f41b0fc326344d2f53c9a
root <root@DESKTOP-L1CLIL1>
2025-01-24 16:13:49 +1100

    add new entry

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log("Hello World")
 
-// let's this is some data we obtained from some API
+// Sample data array from the API
 const dataArray = [
     "Frankenstein",
     "Dracula",
@@ -12,44 +12,72 @@ const dataArray = [
 ]
 
 const costumesContainer = document.getElementById("costumesContainer")
-
 console.log(costumesContainer)
 
 renderData()
 
+function createNewEntry(list, costume) {
+    // Create list items
+    const newCostumeEntry = document.createElement("li")
+    newCostumeEntry.innerText = costume
+
+    // Create and append a remove button
+    const removeButton = document.createElement("button")
+    removeButton.innerText = `Remove ${costume}`
+
+    // Add functionality to the remove button
+    removeButton.addEventListener("click", () => {
+        removeCostumeFromList(list, newCostumeEntry)
+    })
+
+    newCostumeEntry.appendChild(removeButton)
+    list.appendChild(newCostumeEntry)
+}
+
 function renderData() {
     const costumesContainerList = document.createElement("ul")
+    costumesContainerList.id = "costumesContainerList"
 
     dataArray.forEach((costume) => {
-        // lets see what we are dealing with
         console.log(costume)
-
-        // Create list items
-        const newCostumeEntry = document.createElement("li")
-        newCostumeEntry.innerText = costume
-
-        // Create and append a remove button
-        const removeButton = document.createElement("button")
-        removeButton.innerText = `Remove ${costume}`
-
-        // add the functionality to the remove button
-        removeButton.addEventListener("click",() => {
-            removeCustomeFromList(costumesContainerList, newCostumeEntry)
-        })
-
-        
-
-        newCostumeEntry.appendChild(removeButton)
-
-        costumesContainerList.appendChild(newCostumeEntry)
-
-
+        createNewEntry(costumesContainerList, costume)
     })
 
     costumesContainer.appendChild(costumesContainerList)
-
 }
 
-function removeCustomeFromList(list, entry) {
+function removeCostumeFromList(list, entry) {
     list.removeChild(entry)
+}
+
+const formInputButton = document.querySelector("#formInputButton")
+formInputButton.addEventListener("click", (event) => {
+    addCostumeToList(event)
+})
+
+function addCostumeToList(event) {
+    event.preventDefault()
+
+    // Get the form
+    const costumeForm = document.getElementById("costumeInputForm")
+    
+    // Check if the form is valid
+    const isFormValid = costumeForm.checkValidity()
+
+    if (!isFormValid) {
+        costumeForm.reportValidity()
+        return
+    }
+
+    // Get the input field value
+    const newCostumeInput = document.getElementById("costumeInputText")
+
+    const costumesContainerList = document.getElementById("costumesContainerList")
+
+    // Call createNewEntry to add the new costume entry to the list
+    createNewEntry(costumesContainerList, newCostumeInput.value)
+
+    // Clear the input field after adding
+    newCostumeInput.value = ""
+    newCostumeInput.focus()
 }
